dab4d58d425a778356160802c2c8b4f389d2c48c
Gordon D <gordon.draine97@yahoo.com>
2023-03-16 09:55:09 -0400

    adding more to registration forum

--- Components/Resistration.jsx
+++ Components/Resistration.jsx
@@ -9,10 +9,44 @@ const Resistration = () => {
      const handleSubmit = (e) => {
     e.preventDefault();
     // here is the way to handle the sumbmission form 
-
+     };
   return (
-    <div>Resistration</div>
-  )
-}
+    <form onSubmit={handleSubmit} className="bg-white p-8 rounded-lg shadow-md">
+      <h2 className="text-2xl font-bold mb-4">Register</h2>
+      <div className="mb-4">
+        <label
+          htmlFor="firstName"
+          className="block text-gray-700 font-bold mb-2"
+        >
+          First Name
+        </label>
+        <input
+          type="text"
+          id="firstName"
+          className="border rounded-lg px-3 py-2 w-full"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="lastName"
+          className="block text-gray-700 font-bold mb-2"
+        >
+          Last Name
+        </label>
+        <input
+          type="text"
+          id="lastName"
+          className="border rounded-lg px-3 py-2 w-full"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+      </div>
+    </form>
+  );
+};
 
-export default Resistration
+export default Resistration;
