88cd76870f7e41804829af4454a50f7667e9dcd2
Gordon D <gordon.draine97@yahoo.com>
2023-03-18 23:17:43 -0400

    finalizing registration form

--- Components/Resistration.jsx
+++ Components/Resistration.jsx
@@ -45,6 +45,41 @@ const Resistration = () => {
           required
         />
       </div>
+      <div className="mb-4">
+        <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="border rounded-lg px-3 py-2 w-full"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="password"
+          className="block text-gray-700 font-bold mb-2"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="border rounded-lg px-3 py-2 w-full"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Register
+      </button>
     </form>
   );
 };
