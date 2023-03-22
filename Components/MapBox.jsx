abb2e45361a276d72541fa38d1d25c92b926b7f5
Gordon D <gordon.draine97@yahoo.com>
2023-03-21 22:38:53 -0400

    starting the map box component

---
+++ Components/MapBox.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
+
+mapboxgl.accessToken = "YOUR_ACCESS_TOKEN_HERE";
+
+const Map = () => {
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    const initializeMap = ({ setMap, mapContainer }) => {
+      const map = new mapboxgl.Map({
+        container: mapContainer,
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-122.4194, 37.7749],
+        zoom: 12,
+      });
+
+      map.on("load", () => {
+        setMap(map);
+        map.resize();
+      });
+    };
+
+    if (!map) initializeMap({ setMap, mapContainer: mapContainerRef.current });
+  }, [map]);
+
+  const mapContainerRef = useRef(null);
+
+const  = () => {
+  return (
+    
+  )
+}
+
+export default 
