b853e28753ee2b14ae6184acab68012f6dab64ac
Gordon D <gordon.draine97@yahoo.com>
2023-03-28 23:28:43 -0400

    adding hero rough draft

---
+++ Components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+      <div className="nav">
+        <img src="./src/images/tumblr_mwuo4dhY3i1rey868o1_500 - Copy.gif"></img>
+
+        <h3>a better bike company</h3>
+
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+
+        <div>circle goes here for pfp</div>
+
+        <h2>profile</h2>
+      </div>
+    </>
+  );
+}
+
+export default Hero
